bedef1dee99516f58d1f719016607aac47c2f8de
itsferlima <fer.nand.2149@gmail.com>
2024-10-07 22:54:20 +0100

    remove labals add new components

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import HellowWord from './components/HelloWord';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
   return (
     <div className="App">
       <HellowWord/>
+      <Frase></Frase>
       <SayMyName nome="Fernanda"/>
       <SayMyName nome="Daniel"/>
       <Pessoa nome="Fernanda" idade="23" profissao="Desenvolvedora"/>
